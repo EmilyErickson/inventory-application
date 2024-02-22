d15bc8576a4bae954500105beb0944cb1744c64f
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-22 11:22:10 -0600

    Change template library from ejs to pug

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ async function main() {
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/inventory", inventoryRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
