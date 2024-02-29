1672c391e40ac099de0c3ff179d925d2df49ea11
EmilyErickson <emilyerickson2002@gmail.com>
2024-02-29 11:10:00 -0600

    Create admin password requirement

--- models/category.js
+++ models/category.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   category_name: { type: String, required: true, maxLength: 100 },
   category_description: { type: String, minLength: 10 },
+  admin_password: { type: String, required: true },
 });
 
 CategorySchema.virtual("url").get(function () {
